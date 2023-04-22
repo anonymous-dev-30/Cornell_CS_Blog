f272bee3da15ee51bd07778f6eb417e6799fe42b
kotharismeet-3 <smeet.r.kothari@gmail.com>
2023-04-22 10:29:11 +0530

    sanity CMS

---
+++ api/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'jj6fjp3s',
+    dataset: 'production'
+  }
+})
---
+++ api/schemas/comment.ts
@@ -0,0 +1,20 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+    name: 'comment',
+    title: 'Comment',
+    type: 'document',
+    fields: [
+      defineField({
+        name: 'comment',
+        title: 'comment',
+        type: 'string',
+      }),
+      defineField({
+        name: 'post',
+        title: 'Post',
+        type: 'reference',
+        to: {type: 'post'},
+      }),
+    ]
+})
